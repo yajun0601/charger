6f4bed793e77259a41ead0db999078faaeec207e
yajun0601 <yajun0601@gmail.com>
2016-01-21 08:28:12 +0800

    init

---
+++ VerticalProgressBar/src/com/custom/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.custom.activity;
+
+import com.custom.verticalprogressbar.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+}
